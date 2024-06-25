56a43f61c5a2f0cc4236cae31e6b1a6fa0621c2d
felipe443226 <garcestriana226@gmail.com>
2024-06-25 15:41:35 -0500

    se acobo el codigo wasaaaaaaaaa

--- cajero.php
+++ cajero.php
@@ -3,17 +3,16 @@
 
 function menu() {
     echo "1. consultar saldo\n";
-    echo "2. consultar dinero\n";
+    echo "2. depocitar dinero\n";
     echo "3. retirar dinero\n";
 }
 
-$saldo = 1230;
+$saldo = 15478;
 
 while (true){
+
     menu();
     $opcion = readline("elecione un numero :");
-
-
 
     switch ($opcion){
 
@@ -28,9 +27,14 @@ while (true){
             break;
         
             case '3':
-                
+                $retiro = readline("ingrese la cantidad a sacar : ");
+                if ($retirar > $saldo){
+                    echo "no tienes sufisientes saldo para retirar\n";
+                } else {
+                    $saldo -= $retiro;
+                    echo "su retiro fue exsitoso\n";
+                }
+                break;
     }
 }
-
-
 
